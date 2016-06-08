d7fb17613e04ad9d72adc328d105c95f0220a51e
Frank <Frank@FrankdeMacBook-Pro.local>
2016-06-08 18:10:45 +0800

    init

---
+++ UIView+Frame.h
@@ -0,0 +1,30 @@
+//
+//  UIView+Frame.h
+//  UIViewAddition
+//
+//  Created by Frank on 16/6/8.
+//
+//  http://fanwenbin.com
+//
+//  Copyright © 2016年 Frank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Frame)
+
+@property (nonatomic, assign) CGFloat x;
+@property (nonatomic, assign) CGFloat y;
+@property (nonatomic, assign) CGFloat centerX;
+@property (nonatomic, assign) CGFloat centerY;
+@property (nonatomic, assign) CGFloat maxX;
+@property (nonatomic, assign) CGFloat maxY;
+@property (nonatomic, assign) CGFloat width;
+@property (nonatomic, assign) CGFloat height;
+@property (nonatomic, assign) CGSize size;
+@property (nonatomic, assign) CGFloat top;
+@property (nonatomic, assign) CGFloat left;
+@property (nonatomic, assign) CGFloat bottom;
+@property (nonatomic, assign) CGFloat right;
+
+@end
